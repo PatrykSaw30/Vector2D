79713968ec396e58948c37f50c7c5cecc0ae0aca
PatrykSaw30 <sawickiipatrykk@gmail.com>
2026-05-08 14:00:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
-#include "Vector2D.h"
+#include "Point.h"
+using namespace std;
 
 int main() {
-    Vector2D v1(10.0, 5.0);
-    Vector2D v2(2.0, 2.0);
 
-    Vector2D v3 = v1 + v2;
-    Vector2D v4 = v1 - v2;
+    Point p1(10.0f, 20.0f, 30.0f);
+    Point p2(2.0f, 4.0f, 5.0f);
 
-    v1 += v2;      
-    v1 *= 2.0;     
+    cout << "p1: " << p1 << endl;
+    cout << "p2: " << p2 << endl;
 
-    std::cout << "Result v1: " << v1 << std::endl;
-    std::cout << "Result v3 (v1+v2): " << v3 << std::endl;
+    cout << "(p1 + p2): " << p1 + p2 << endl;
 
-    v1.y = 100.0;
+    cout << "(p1 - p2): " << p1 - p2 << endl;
+
+    cout << "(p1 * p2): " << p1 * p2 << endl;
+
+    cout << "(p1 / p2): " << p1 / p2 << endl;
+
+
+    Point p3(1.0f, 1.0f, 1.0f);
+    cout << "p3: " << p3 << endl;
+
+    p3 += 5.0f;
+    cout << "(p3 += 5.0f): " << p3 << endl;
+
+    p3 *= 2.0f;
+    cout << "(p3 *= 2.0f): " << p3 << endl;
+
+    p3 -= 1.0f;
+    cout << "(p3 -= 1.0f): " << p3 << endl;
+
+    p3 /= 0.5f;
+    cout << "(p3 /= 0.5f): " << p3 << endl;
 
     return 0;
 }
